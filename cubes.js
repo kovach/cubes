979abd2ddee2ccfa403901733b796ad0fce44ccf
Scott Kovach <kovach@mit.edu>
2014-04-20 23:19:21 -0700

    new block

--- cubes.js
+++ cubes.js
@@ -1,76 +1,109 @@
-//// Scene Stuff
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+randcolor = function() {
+  var r = Math.random();
+  return r * 0xffffff;
+}
+green = function() { return 0x00ff00; }
+blue = function() { return 0x0000ff; }
 
 //// Cubes
-// Parameters/Util
-var n = 6;
-var geometry = new THREE.CubeGeometry(1,1,1);
+initCubes = function(n, dilation, color) {
+  return function(scene, camera, renderer) {
 
-normalizePos = function(xyz) {
-  return [ (xyz[0] - n/2) * 0.7
-         , (xyz[1] - n/2) * 0.7
-         , (xyz[2] - n/2) * 0.7
-         ];
+    var geometry = new THREE.CubeGeometry(1,1,1);
+
+    normalizePos = function(xyz) {
+      return [ (xyz[0] - n/2) * dilation
+        , (xyz[1] - n/2) * dilation
+        , (xyz[2] - n/2) * dilation
+        ];
+    }
+
+    makeCube = function(i) {
+      var material = new THREE.MeshLambertMaterial( { color: color() } );
+      var cube = new THREE.Mesh( geometry, material );
+
+      var z = Math.floor(i / (n*n));
+      var y = Math.floor(i / n) % n;
+      var x = i % n;
+      var xyz = normalizePos([x,y,z]);
+
+      var isEdge = function(x) {
+        return x == 0 || x == n-1;
+      }
+      if (! (isEdge(x) || isEdge(y) || isEdge(z))) {
+        return undefined;
+      } else {
+        scene.add(cube);
+        cube.position.set(xyz[0],xyz[1],xyz[2]);
+        return cube;
+      }
+    }
+
+    // Make the Cubes
+
+    // some makeCube calls will return undefined
+    makeCubes = function() {
+      return _.filter(_.map(_.range(n*n*n), makeCube), function(x) { return x; })
+    }
+
+    //// Camera
+    makeCamera = function() {
+      camera.position.z = 11;
+      controls = new THREE.OrbitControls( camera, renderer.domElement );
+    }
+
+    //// Light
+    addLight = function (xyz) {
+      var xyz = normalizePos(xyz);
+      var light = new THREE.PointLight(0x888888);
+      light.position.set(xyz[0], xyz[1], xyz[2]);
+      scene.add(light);
+    }
+
+    addLights = function() {
+      var m = n * 1.5;
+      _.each([[m,0,0], [0,m,0], [0,0,m], [-m,0,0], [0,-m,0], [0,0,-m]], addLight);
+      //addLight([ n*10,0,0]);
+      //addLight([-n*10,0,0]);
+    }
+
+
+    var cubes = makeCubes();
+    makeCamera();
+    addLights();
+
+    return cubes;
+  }
 }
 
-makeCube = function(i) {
-  var r = Math.random();
-  var color = r * 0xffffff;
-  var green = 0x00ff00;
-  var material = new THREE.MeshLambertMaterial( { color: green } );
-  return new THREE.Mesh( geometry, material );
-}
-
-addCube = function(cube, i) {
-    scene.add(cube);
-
-    var z = Math.floor(i / (n*n));
-    var y = Math.floor(i / n) % n;
-    var x = i % n;
-    var xyz = normalizePos([x,y,z]);
-    cube.position.set(xyz[0],xyz[1],xyz[2]);
-}
-
-// Make the Cubes
-cubes = _.map(_.range(n*n*n), makeCube);
-_.each(cubes, addCube);
-
-//// Camera
-camera.position.z = 11;
-controls = new THREE.OrbitControls( camera, renderer.domElement );
-
-//// Light
-addLight = function (xyz) {
-  var xyz = normalizePos(xyz);
-  var light = new THREE.PointLight(0x888888);
-  light.position.set(xyz[0], xyz[1], xyz[2]);
-  scene.add(light);
-}
-
-var m = n * 1.5;
-_.each([[m,0,0], [0,m,0], [0,0,m], [-m,0,0], [0,-m,0], [0,0,-m]], addLight);
-//addLight([ n*10,0,0]);
-//addLight([-n*10,0,0]);
-
-
-function render() {
-  requestAnimationFrame(render);
-
+renderCubes = function(cubes) {
   _.each(cubes, function(cube) {
+    var r = Math.random();
 
-      var r = Math.random();
-
-      cube.rotation.x += r * 0.1;
-      cube.rotation.y += r * 0.1;
-      });
-
-  //geometry.computeFaceNormals();
-  //geometry.computeVertexNormals(); 
-
-  renderer.render(scene, camera);
+    cube.rotation.x += r * 0.1;
+    cube.rotation.y += r * 0.1;
+  });
 }
-render();
+
+
+
+initGL = function(initFn, renderFn) {
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  var data = initFn(scene, camera, renderer);
+
+  var render = function() {
+    requestAnimationFrame(render);
+
+    renderFn(data);
+
+    renderer.render(scene, camera);
+  }
+
+  render();
+}
+
